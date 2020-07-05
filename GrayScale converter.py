90807073044f4689a364728fb7e0bb7f04c01088
abdallah mahdy <abdallahmahdyyy@gmail.com>
2020-07-05 04:33:44 +0200

    fix few bags
    - if image folder not exists
    fix file path bug

--- GrayScale converter.py
+++ GrayScale converter.py
@@ -6,6 +6,11 @@ import sys
 image_folder = sys.argv[1]
 image_output = sys.argv[2]
 
+# check if the input folder is exists or not
+if(not os.path.exists(image_folder)):
+    raise FileNotFoundError(" The images folder not exists ")
+
+
 # check if the output folder is exists or not if not create it
 if(not os.path.exists(image_output)):
     os.makedirs(image_output)
@@ -13,9 +18,9 @@ if(not os.path.exists(image_output)):
 # loop through all images in image_folder
 for filename in os.listdir(image_folder):
     if(filename.endswith("jpg") or filename.endswith("png")):
-        img = Image.open(f"{image_folder}{filename}")
+        img = Image.open(f"{image_folder}/{filename}")
         name = os.path.splitext(filename)[0]
         # convert image to greyscale and save it
         img = img.convert("L")
-        img.save(f"{image_output}{name}.png", 'png')
+        img.save(f"{image_output}/{name}.png", 'png')
         print("done")
